2952ef2c22bbb84e07463bde31d80087b25854ad
xsakurait <60243830+xsakurait@users.noreply.github.com>
2021-09-21 00:51:01 +0900

    Add files via upload

---
+++ conda.py
@@ -0,0 +1,9 @@
+仮想環境作成　conda create -n 環境名　pyてょん＝version
+
+conda activate 環境名
+conda deactivate パッケージをディアクティベート（仮想環境から抜ける）する
+conda install パッケージ名
+conda uninstall パッケージ名
+conda env list 仮想環境の場所
+conda list アクティベートした環境のパッケージ一覧
+conda remove -n 環境名 (--all)仮想環境を削除
